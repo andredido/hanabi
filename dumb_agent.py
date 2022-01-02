b0bdf0905af6189ecfbf5eff95b8be613011792f
andreadido <s287639@studenti.polito.it>
2022-01-02 10:36:33 +0100

    GameManager

--- dumb_agent.py
+++ dumb_agent.py
@@ -1,1 +1,13 @@
+import GameManager
+from threading import Thread
 
+gm1 = GameManager.GameManager('127.0.0.1', '1024', 0)
+command = ''
+while True:
+    print('\nType ready when all the players are connected')
+    command = input()
+    if command == "ready":
+        break
+gm1.ready()
+while gm1.status == gm1.statuses[1]:
+    gm1.current_state()
--- my_agent.py
+++ my_agent.py
@@ -1,31 +1,11 @@
-from sys import argv, stdout
-from threading import Thread, Lock, Condition
-import GameData
-import socket
-from constants import *
-from collections import deque
+import GameManager
+from threading import Thread
 
-class IntelligenceAgent:
-    def __init__(self, ip=HOST, port=PORT, nmyAgent=0):
-        self.statuses = ["Lobby", "Game", "GameHint"]
-        self.status = self.statuses[0]
-        self.playerName = "myAgent"
-        if nmyAgent > 0:
-            self.playerName += str(nmyAgent)
-        
-        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        request = GameData.ClientPlayerAddData(self.playerName)
-        self.s.connect((ip, port))
-        self.s.send(request.serialize())
-        data = self.s.recv(DATASIZE)
-        data = GameData.GameData.deserialize(data)
-        if type(data) is GameData.ServerPlayerConnectionOk:
-            print("Connection accepted by the server. Welcome " + self.playerName)
-        stdout.flush()
-
-        self.lock = Lock()
-        self.cv = Condition()
-        self.run = True
-    
-    def __del__(self):
-        self.s.close()
+gm = GameManager.GameManager('127.0.0.1', '1024', 1)
+command = ''
+while True:
+    print('\nType ready when all the players are connected')
+    command = input()
+    if command == "ready":
+        break
+gm.ready()
